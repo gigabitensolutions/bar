ec97c22450c302b1b43d38ab2e99c744712722c6
Gigabiten IT Solutions <gigabitensolucoesti@gmail.com>
2025-09-05 14:34:02 -0300

    rollback test

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,4 +1,5 @@
 // assets/firebase-config.js
+// Ajuste TENANT_ID se quiser separar por unidade/loja.
 window.FIREBASE_CONFIG = {
   BACKEND_URL: "https://us-central1-comanda-bar-e260d.cloudfunctions.net/api",
   TENANT_ID: "default"
